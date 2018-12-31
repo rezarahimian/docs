a9a83dc6faffe9c3e76216f2934a722cc84f26e8
rezarahimian <22454559+rezarahimian@users.noreply.github.com>
2018-12-31 18:57:28 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 extensions = ['sphinxcontrib.bibtex','sphinx.ext.mathjax']
 master_doc = 'index'
-project = u'Project docs'
+project = u'Project'
 copyright = u'Public'
 author = u'-'
 show_authors = True
