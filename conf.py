aa356df5e7225be8821b332031d928c2ef2fcb25
rezarahimian <22454559+rezarahimian@users.noreply.github.com>
2018-12-31 18:33:44 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -4,27 +4,13 @@ project = u'Project documentations'
 copyright = u'Public'
 author = u'-'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-
-
-# -- Options for HTMLHelp output ------------------------------------------
-
-# Output file base name for HTML help builder.
-# htmlhelp_basename = 'oemof_heatdoc'
-
-
-# -- Options for LaTeX output ---------------------------------------------
-
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'oemof_heat.tex', u'oemof\\_heat Documentation',
-     u'c-moeller, jnnr', 'manual'),
-]
+# latex_documents = [
+#    (master_doc, 'oemof_heat.tex', u'oemof\\_heat Documentation',
+#     u'c-moeller, jnnr', 'manual'),
+# ]
 
 
 # -- Options for manual page output ---------------------------------------
