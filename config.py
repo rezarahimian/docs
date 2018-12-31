b913a7250a2ea37f04ae2346bb1168e5f2b3d0f4
rezarahimian <22454559+rezarahimian@users.noreply.github.com>
2018-12-31 00:10:32 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,1 @@
 show_authors = True
-
-extensions = [-
-    'easydev.copybutton',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.coverage',
-    'sphinx.ext.graphviz',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.pngmath',
-    'sphinx.ext.ifconfig',
-    'matplotlib.sphinxext.only_directives',
-    'matplotlib.sphinxext.plot_directive',
- ]
