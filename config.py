c6cad8da456f9ed1e227d009adc18908f347c30d
rezarahimian <22454559+rezarahimian@users.noreply.github.com>
2018-12-31 00:07:27 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,17 @@
-highlight_language = 'Solidity'
 show_authors = True
+
+extensions = [-
+    'easydev.copybutton',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.coverage',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.pngmath',
+    'sphinx.ext.ifconfig',
+    'matplotlib.sphinxext.only_directives',
+    'matplotlib.sphinxext.plot_directive',
+ ]
