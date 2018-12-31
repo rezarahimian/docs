f4d4d7045cea1e78b27f8e869d3e2d87d35578c5
rezarahimian <22454559+rezarahimian@users.noreply.github.com>
2018-12-31 18:22:45 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@
 extensions = ['sphinxcontrib.bibtex','sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
