2db2c4a87061f59615b58de43a8eefcb5c0c1b02
rezarahimian <22454559+rezarahimian@users.noreply.github.com>
2018-12-31 18:19:27 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 # General information about the project.
 # project = u'oemof_heat'
 # copyright = u'2018, c-moeller, jnnr'
-# author = u'c-moeller, jnnr'
+ author = u'c-moeller, jnnr'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
