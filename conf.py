ecff711166d41d9f583499d0a843f3b2e782482a
rezarahimian <22454559+rezarahimian@users.noreply.github.com>
2018-12-31 22:06:42 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -7,7 +7,6 @@ show_authors = True
 html_theme = "agogo"
 html_theme_options = {
   "body_min_width" : "60%",
-  "documentwidth" : "60em",
   "sidebarwidth" : "200px",
   "rightsidebar" : "false",
   "stickysidebar" : "true",
